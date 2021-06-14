89ee15d69e31a35cad2c721835a514b705561e0e
Your Name <epyon85@gmail.com>
2021-06-14 18:17:26 -0400

    settiongs page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { NavBar } from './components/navbar/NavBar';
 import Home from './components/home/Home';
 import Single from './components/pages/single/Single';
 import User from './components/pages/user/User';
+import Settings from './components/pages/settings/Settings';
 
 function App() {
   return (
     <>
       <NavBar />
-      <User />
+      <Settings />
     </>
     
   );
